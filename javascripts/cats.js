54c3c1351fb74f087dd8e6352b5c8fc6e839584c
holmanhub <holman.eric85@gmail.com>
2017-10-03 10:50:05 -0500

    added api and quantity function

---
+++ javascripts/cats.js
@@ -0,0 +1,13 @@
+// "use strict";
+
+let cats =[];
+
+// $.get('https://random-dogs-api.herokuapp.com/cats/NUMBEROFCATZZZZZ')
+
+
+
+const getCats = () => {
+	return cats;
+};
+
+// module.exports = {getCats};
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,4 @@
 "use strict";
+
+require('./events.js');
+require('./cats.js');
